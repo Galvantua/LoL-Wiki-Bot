a2f420ccfa7cc5c8f9ed0d524c9a50154201ab6d
eramsorgr <nikos@eramsorgr.me>
2021-09-07 19:35:52 +0300

    added command handler

--- bot.js
+++ bot.js
@@ -13,5 +13,25 @@ bot.once('ready', async () => {
    console.log(`woah the bot is running`); 
 });
 
+bot.on('message', async (message) => {
+
+   //Ignore bot messages
+   if(message.author.bot) return;
+   if(!message.content.startsWith(config.prefix)) return;
+
+   //Create a command handler
+   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+   const command = args.shift().toLowerCase();
+
+   //Create a command handler
+   try {
+      let commandFile = require(`./commands/${command}.js`);
+      commandFile.run(bot, message, args);
+   } catch (err) {
+      console.error(err);
+   };
+
+});
+
 //Login to gateway
 bot.login(config.token); 
