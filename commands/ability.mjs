ca1313a3a80215c649f6e93baab8e746f7ac1040
Galvantua <34282639+Galvantua@users.noreply.github.com>
2021-09-07 13:37:48 -0400

    Update ability.mjs

--- commands/ability.mjs
+++ commands/ability.mjs
@@ -1,1 +1,22 @@
+export async function run(bot, core, message, args) {
 
+    const Http = new XMLHttpRequest();
+    const url='https://leagueoflegends.fandom.com/wiki/${}/LoL';
+    Http.open("GET", url);
+	Http.send();
+
+    Http.onreadystatechange = (e) => {
+        console.log(Http.responseText)
+    }
+  
+    //create an embed
+    const embed = new core.MessageEmbed()
+    embed.setTitle("Pong!")
+    embed.setColor('#01eee8') 
+    embed.addField(`🖥️ WebSocket`, `**${bot.ws.ping}**ms`)
+    //create a red hex color embed
+
+    //send the embed
+    await message.channel.send({ embeds: [embed]});
+
+}
