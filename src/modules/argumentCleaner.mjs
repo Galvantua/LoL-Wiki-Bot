36453b08681b9a60ab0f7b259b21601413b99b94
eramsorgr <nikos@eramsorgr.me>
2021-09-08 23:20:12 +0300

    support for more arg types

--- src/modules/argumentCleaner.mjs
+++ src/modules/argumentCleaner.mjs
@@ -16,11 +16,44 @@ export async function nameAbility(args) {
         return {
             championName: championName,
             championAbility: args[1]
+        };
+
+    } else if (args.length === 3) {
+
+        let championName = args[0].toLowerCase() + args[1].toLowerCase();
+        let championAbility = args[2].toLowerCase();
+
+        championName = championName.replace(/['"^_.]/gm, '');
+
+        if (!/[qwerp]/gm.test(championAbility) || !championAbility === "passive") {
+            return "Invalid ability";
+        };
+
+        return {
+            championName: championName,
+            championAbility: args[1]
         }
+
+    } else if (args.length === 4) {
+
+        let championName = args[0].toLowerCase() + args[1].toLowerCase() + args[2].toLowerCase();
+        let championAbility = args[3].toLowerCase();
+
+        championName = championName.replace(/['"^_.]/gm, '');
+
+        if (!/[qwerp]/gm.test(championAbility) || !championAbility === "passive") {
+            return "Invalid ability";
+        };
+
+        return {
+            championName: championName,
+            championAbility: args[1]
+        };
+
     };
 
 };
 
-// let testarray = ["kog'maw", "passive"]
+// let testarray = ["kog", "maw", "passive"]
 // let testability = await nameAbility(testarray)
 // console.log(testability)
