bfaf36003e6e5110a70778fc527cc2a38c69d838
Galvantua <34282639+Galvantua@users.noreply.github.com>
2021-09-23 20:47:43 -0400

    import abilityCleanerV2 to ability.js, move args around for ease of use, fix array calls to match args

--- commands/ability.mjs
+++ commands/ability.mjs
@@ -1,29 +1,29 @@
+import { nameAbilityTEST } from "/src/modules/argumentCleanerV2.js";
 export async function run(bot, core, message, args) {
 	//requires args to be ["ability", "champion", "{q,w,e,r,p}"
 	
 	//create parser for HTML
 	const parser = new DOMParser();
 	const document, abilityMain, abilityCode, abilityDetails, abilityNumber;
-	
-	if (args[1] == "kogmaw") {
-		args[1] = "kog'maw";
-	} //add exceptions here
+	args.shift();
 
-	if (args[2].lowerCase() === "p" || args[2].toLowerCase() === "passive") {
+	args = nameAbilityTEST(args);
+
+	if (args[1].lowerCase() === "p" || args[1].toLowerCase() === "passive") {
 		abilityCode = "innate";
 		abilityNumber = 0;
 	} else {
-		args[2].toLowerCase();
-		if (args[2].toLowerCase() === "q") {
+		args[1].toLowerCase();
+		if (args[1].toLowerCase() === "q") {
 			abilityCode = "q";
 			abilityNumber = 1;
-		} else if (args[2].toLowerCase() === "w") {
+		} else if (args[1].toLowerCase() === "w") {
 			abilityCode = "w";
 			abilityNumber = 2;
-		} else if (args[2].toLowerCase() === "e") {
+		} else if (args[1].toLowerCase() === "e") {
 			abilityCode = "e";
 			abilityNumber = 3;
-		} else if (args[2].toLowerCase() === "r") {
+		} else if (args[1].toLowerCase() === "r") {
 			abilityCode = "r";
 			abilityNumber = 4;
 		} else {
@@ -32,7 +32,7 @@ export async function run(bot, core, message, args) {
 	};
 	
     const Http = new XMLHttpRequest(); //create new request
-    const url='https://leagueoflegends.fandom.com/wiki/${args[1]}/LoL'; 
+    const url=`https://leagueoflegends.fandom.com/wiki/${args[0]}/LoL`; 
     Http.open("GET", url);
 	Http.send();
 
--- src/modules/argumentCleanerV2.mjs
+++ src/modules/argumentCleanerV2.mjs
@@ -1,54 +1,50 @@
-
 export async function nameAbilityTEST(args) {
-    let excList = {
-    'chogath':"Cho'Gath",
-    'kaisa':"Kai'Sa",
-    'khazix':"Kha'Zix",
-    'kogmaw':"Kog'Maw",
-    'reksai':"Rek'Sai",
-    'velkoz':"Vel'Koz",
-    'aurelionsol':"Aurelion_Sol",
-    'asol':"Aurelion_Sol",
-    'mundo':"Dr._Mundo",
-    'drmundo':"Dr._Mundo",
-    'jarvan':"Jarvan_IV",
-    'jarvaniv':"Jarvan_IV",
-    'fortune':"Miss_Fortune",
-    'yi':"Master_Yi",
-    'nunuwillump':"Nunu",
-    'nunuandwillump':"Nunu",
-    'tf':"Twisted_Fate",
-    'xin':"Xin_Zhao",
-  }; //list of exceptions 
+	let excList = {
+		chogath: "Cho'Gath",
+		kaisa: "Kai'Sa",
+		khazix: "Kha'Zix",
+		kogmaw: "Kog'Maw",
+		reksai: "Rek'Sai",
+		velkoz: "Vel'Koz",
+		aurelionsol: "Aurelion_Sol",
+		asol: "Aurelion_Sol",
+		mundo: "Dr._Mundo",
+		drmundo: "Dr._Mundo",
+		jarvan: "Jarvan_IV",
+		jarvaniv: "Jarvan_IV",
+		fortune: "Miss_Fortune",
+		yi: "Master_Yi",
+		nunuwillump: "Nunu",
+		nunuandwillump: "Nunu",
+		tf: "Twisted_Fate",
+		xin: "Xin_Zhao",
+	}; //list of exceptions
 
-  //regex and return taken from eramsorgr. I'm too lazy to type those
-  championAbility = args[args.length-1].toLowerCase()
-  if(!/[qwerp]/gm.test(championAbility) || !championAbility === "passive"){
-    return "Invalid ability";
-  };              //checks if the input is valid
+	//regex and return taken from eramsorgr. I'm too lazy to type those
+	championAbility = args[args.length - 1].toLowerCase();
+	if (!/[qwerp]/gm.test(championAbility) || !championAbility === "passive") {
+		return "Invalid ability";
+	} //checks if the input is valid
 
-  args = args.filter((a) => a); //remove the empty arrays
-  let championName = "";
-  for(let i = 0; i<args.length-1; i++){
-    args[i] = args[i].toLowerCase().replace(/['"^_.\s]/gm, '');
-    args[i] = args[i].charAt(0).toUpperCase()+args[i].slice(1);
-    championName += args[i]+"_";
-  };               //reformat the champ name
+	args = args.filter((a) => a); //remove the empty arrays
+	let championName = "";
+	for (let i = 0; i < args.length - 1; i++) {
+		args[i] = args[i].toLowerCase().replace(/['"^_.\s]/gm, "");
+		args[i] = args[i].charAt(0).toUpperCase() + args[i].slice(1);
+		championName += args[i] + "_";
+	} //reformat the champ name
 
-  championName = championName.slice(0, -1);   //removes the excess underscore
-  let nameCheck = championName.replace(/[_]/gm,'').toLowerCase();
-  if(excList.hasOwnProperty(nameCheck)){
-    championName = excList[nameCheck]; 
-  }               //check the exception list
+	championName = championName.slice(0, -1); //removes the excess underscore
+	let nameCheck = championName.replace(/[_]/gm, "").toLowerCase();
+	if (excList.hasOwnProperty(nameCheck)) {
+		championName = excList[nameCheck];
+	} //check the exception list
 
-  //console.log(championName)  
-  //console.log(championAbility);
-
-  return{
-    championName: championName,
-    championAbility: championAbility
-  };
-};
+	//console.log(championName)
+	//console.log(championAbility);
+	args = [championName, championAbility];
+	return args; // need to rewrite to spit out array
+}
 
 //let testarray = ["kO g_.","","", "mAW", "Passive"];
 //let testarray2 = ["tWiSTed","","", "fAte", "Q"];
