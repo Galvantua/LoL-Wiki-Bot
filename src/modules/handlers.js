6ff22b4c7d31a2f096771e146dc48dbb7d9d6655
Aurora Takemi <98479040+auroraisluna@users.noreply.github.com>
2022-09-09 00:40:19 +0300

    code cleanup and some comments

--- src/modules/handlers.js
+++ src/modules/handlers.js
@@ -1,19 +1,20 @@
-const handler = function () {};
+const fetch = require("node-fetch");
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
-const fetch = require("node-fetch");
-const {
-	ActionRowBuilder,
-	ButtonBuilder,
-	ButtonStyle,
-	EmbedBuilder,
-} = require("discord.js");
+const {	ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder } = require("discord.js");
 
+//Define the handler
+const handler = function () {};
+
+//Special handler for aphelios weapon system 
 handler.apheliosHandler = async function (abilityLetter, interaction) {
+
+	//Prevent discord from ending the interaction
 	await interaction.deferReply();
+
 	let document, aphAbilities;
-	let myEmbeds = [];
-	let buttons = [];
+	let myEmbeds, buttons = [];
+
 	let abilityProperties = [
 		"cast time",
 		"target range",
@@ -24,6 +25,8 @@ handler.apheliosHandler = async function (abilityLetter, interaction) {
 		"effect radius",
 		"width",
 	];
+
+	//Emojis for weapons and 'props'
 	let emojis = [
 		"<:Aphelios_The_Hitman_and_the_Seer:1012902419081986048>",
 		"<:Aphelios_Weapons_of_the_Faithful:1012902419925061663>",
@@ -43,7 +46,8 @@ handler.apheliosHandler = async function (abilityLetter, interaction) {
 		aphAbilities = [0, 2, 4, 6, 8, 10];
 	} else {
 		aphAbilities = [1, 3, 5, 7, 9, 11];
-	}
+	};
+
 	const url = `https://leagueoflegends.fandom.com/wiki/Aphelios/LoL`;
 	const request = await fetch(url).catch((err) => {
 		console.log(err);
@@ -56,47 +60,62 @@ handler.apheliosHandler = async function (abilityLetter, interaction) {
 	});
 
 	document = dom.window.document;
-	let i = 0;
-	for (const aphAbility of aphAbilities) {
-		ability = document.getElementsByClassName("ability-info-container")[
-			aphAbility
-		];
+
+	for (let i = 0; i < aphAbilities.length; i++) {
+
+		let aphAbility = aphAbilities[i];
+
+		ability = document.getElementsByClassName("ability-info-container")[aphAbility];
 
 		const embed = new EmbedBuilder();
 
-		let abilityHeader =
-			ability.getElementsByClassName("mw-headline")[0].textContent;
+		let abilityHeader = ability.getElementsByClassName("mw-headline")[0].textContent;
 
 		embed.setTitle(`**${abilityHeader}**`);
+
 		let abilityStats = ability.getElementsByTagName("aside")[0];
 
 		if (abilityStats) {
+
 			for (let i = 0; i < abilityProperties.length; i++) {
+
 				const element = abilityStats.querySelector(
 					`div[data-source="${abilityProperties[i]}"]`
 				);
+
 				if (element) {
+
 					const elementText = element.textContent.split(":");
+
 					embed.addFields({
 						name: `${elementText[0].trim()}`,
 						value: `${elementText[1].trim()}`,
 						inline: true,
 					});
-				}
-			}
-		}
+
+				};
+			};
+
+		};
+
 		//grabs the array of tables in the ability
 		let abilityTables = ability.getElementsByTagName("table");
 
 		//process the tables in the array and create fields for each subtable
 		for (let i = 0; i < abilityTables.length; i++) {
+
 			const table = abilityTables[i];
+
 			const subTables = table.getElementsByTagName("dl");
+
 			for (let i = 0; i < subTables.length; i++) {
+
 				const subTable = subTables[i];
 				const subTableHeaders = subTable.getElementsByTagName("dt");
 				const subTableData = subTable.getElementsByTagName("dd");
+
 				for (let i = 0; i < subTableHeaders.length; i++) {
+
 					const header = subTableHeaders[i].textContent;
 					const data = subTableData[i].textContent;
 
@@ -105,23 +124,29 @@ handler.apheliosHandler = async function (abilityLetter, interaction) {
 						value: `${data.trim()}`,
 						inline: true,
 					});
-				}
-			}
-		}
+
+				};
+			};
+		};
 
 		let abilityDetails = ability.querySelectorAll("p, ul");
 		let detailText = "";
+
 		for (let i = 0; i < abilityDetails.length; i++) {
+
 			const detail = abilityDetails[i];
 			detailText = detail.textContent;
 
 			if (detailText) {
+
 				embed.addFields({
 					name: `​`,
 					value: `${detailText}`,
 				});
-			}
-		}
+
+			};
+
+		};
 
 		let abilityImage = ability
 			.getElementsByTagName("img")[0]
@@ -131,13 +156,16 @@ handler.apheliosHandler = async function (abilityLetter, interaction) {
 		myEmbeds.push(embed);
 
 		buttons.push(
+
 			new ButtonBuilder()
 				.setCustomId(i.toString())
 				.setStyle(ButtonStyle.Secondary)
 				.setEmoji(emojis[aphAbility])
+
 		);
-		i++;
-	}
+
+	};
+
 	return {
 		embeds: myEmbeds,
 		buttons: buttons,
