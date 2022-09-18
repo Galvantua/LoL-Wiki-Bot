36309f34b413c62f1bff7cc53f4ab97c619a4260
Galvantua <galvantua@gmail.com>
2022-09-18 16:05:25 -0400

    add item commands

---
+++ src/commands/item.mjs
@@ -0,0 +1,62 @@
+import { SlashCommandBuilder, EmbedBuilder } from "discord.js";
+import fetch from "node-fetch";
+import { findChampionName, findAbilityName } from "../modules/nameFinders.mjs";
+
+export const information = {
+	name: "item",
+	description: "Queries Item info from the LoL wiki",
+};
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName(`${information.name}`)
+		.setDescription(`${information.description}`)
+		.addStringOption((option) =>
+			option
+				.setName("item")
+				.setDescription("Item's Name")
+				.setRequired(true)
+		),
+
+	async execute(interaction) {
+		await interaction.deferReply();
+
+		const item = interaction.options.getString("item");
+		const itemId = await tests.nameItemTEST(item, interaction);
+		const embed = new EmbedBuilder();
+
+		const request = await fetch(
+			`https://cdn.merakianalytics.com/riot/lol/resources/latest/en-US/items/${itemId}.json`
+		).catch((err) => {
+			interaction.editReply("Please choose a valid item name");
+			return;
+		});
+
+		const body = await request.text();
+
+		let bodyJSON;
+		try {
+			bodyJSON = JSON.parse(body);
+		} catch (error) {
+			console.log(error);
+			interaction.editReply("**Please choose a valid Item name**");
+			return;
+		}
+
+		embed.setTitle(bodyJSON.name).setThumbnail(bodyJSON.icon);
+
+		for (const stat in bodyJSON.stats) {
+			for (const type in bodyJSON.stats[stat]) {
+				console.log(bodyJSON.stats[stat][type]);
+				if (bodyJSON.stats[stat][type] !== 0.0) {
+					embed.addFields({
+						name: `${type} ${stat}`,
+						value: `${bodyJSON.stats[stat][type]}`,
+					});
+				}
+			}
+		}
+
+		await interaction.editReply({ embeds: [embed] });
+	},
+};
